c3334b9f5729f5be7b2c6cb8e58fc6f40be12fc3
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-16 13:59:43 +0300

    Improve 404

--- gateways/frontend/src/Application/NotFoundHandler.php
+++ gateways/frontend/src/Application/NotFoundHandler.php
@@ -4,20 +4,24 @@ declare(strict_types=1);
 
 namespace App\Gateways\Frontend\Application;
 
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Yii\View\ViewRenderer;
 
 final class NotFoundHandler implements RequestHandlerInterface
 {
     public function __construct(
-        private ResponseFactoryInterface $responseFactory,
+        private ViewRenderer $viewRenderer,
     ) {
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return $this->responseFactory->createResponse(404);
+        return $this->viewRenderer
+            ->withViewPath(__DIR__)
+            ->withLayout('@root/gateways/frontend/layout/error.php')
+            ->render('not-found.php')
+            ->withStatus(404);
     }
 }
---
+++ gateways/frontend/src/Application/not-found.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @var \Yiisoft\View\WebView $this
+ */
+
+$this->setTitle('Страница не найдена');
+?>
+<h1>Страница не найдена</h1>
