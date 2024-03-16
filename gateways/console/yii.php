87e39098b696d79b6cf737ff25c837c42bb2962d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-16 13:14:10 +0300

    Extract bootstrap

--- gateways/console/yii.php
+++ gateways/console/yii.php
@@ -2,17 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Core\Infrastructure\Environment;
 use Yiisoft\Yii\Runner\Console\ConsoleApplicationRunner;
 
-$root = dirname(__DIR__, 2);
-
-require_once  $root . '/vendor/autoload.php';
-
-Environment::prepare();
+require_once dirname(__DIR__) . '/bootstrap.php';
 
 (new ConsoleApplicationRunner(
-    rootPath: $root,
+    rootPath: dirname(__DIR__, 2),
     debug: $_ENV['YII_DEBUG'],
     checkEvents: $_ENV['YII_DEBUG'],
     environment: $_ENV['YII_ENV']
