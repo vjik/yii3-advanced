a9cb8226bd7945eff9bc37dfc6aa1ee160e5fc92
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-16 13:24:57 +0300

    add nginx for frontend

---
+++ gateways/frontend/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Yii\Runner\Http\HttpApplicationRunner;
+
+require_once dirname(__DIR__, 2) . '/bootstrap.php';
+
+(new HttpApplicationRunner(
+    rootPath: dirname(__DIR__, 3),
+    debug: $_ENV['YII_DEBUG'],
+    checkEvents: $_ENV['YII_DEBUG'],
+    environment: $_ENV['YII_ENV']
+))->run();
